9226dee3ba8b51eb38eee127772b146523099e4f
Matthew Thorley <padwasabimasla@gmail>
2015-11-06 19:42:58 -0700

    refactor

--- bible-genius.js
+++ bible-genius.js
@@ -5,12 +5,12 @@ function search(needle, haystack) {
   console.log("searching for " + needle)
   for (i=0; i < keys.length; i++) {
     var key = keys[i]
-    var line = haystack[key]
-    var result = {}
-    result.text = line
+    var lineOfText = haystack[key]
 
-    if (regex.test(line)) {
-      console.log("matched " + line)
+    if (regex.test(lineOfText)) {
+      var result = {}
+      result.location = key
+      result.text = lineOfText
       results.push(result)
     }
   }
@@ -30,7 +30,7 @@ function display(results) {
   var searchResults = document.getElementById("searchResults")
   for (i = 0; i < results.length; i++) {
     var p = document.createElement("p")
-    var txt = document.createTextNode(results[i].text)
+    var txt = document.createTextNode(results[i].text )
     p.appendChild(txt)
     searchResults.appendChild(p)
     console.log(results[i])
