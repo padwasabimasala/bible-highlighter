a2a582245dc5e5887d8903bba85b9dd3ff7bd183
Matthew Thorley <padwasabimasla@gmail>
2015-11-14 15:18:19 -0700

    add bible parser

---
+++ parse.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+require 'json'
+
+class Book
+  def initialize name
+    @name = name
+    @verses = {}
+  end
+
+  def start_verse line
+    @last_verse = line.slice(0,7)
+    @verses[@last_verse] = line.slice(8,line.length).strip.to_s
+  end
+
+  def extend_verse line
+    @verses[@last_verse] += line.slice(1,line.length).strip
+  end
+
+  def to_json opts
+    { @name => @verses }.to_json
+  end
+end
+
+books = {}
+text = File.open('NIV')
+text.each do |line|
+  case line
+  when /^Book \w+ (.*)/
+    $book = Book.new $1
+    books[$1] = $book
+  when /^[^\t]/
+    $book.start_verse line
+  when /^\t/
+    $book.extend_verse line
+  else
+    STDERR.puts :err
+    exit 1
+  end
+end
+
+puts books.to_json
